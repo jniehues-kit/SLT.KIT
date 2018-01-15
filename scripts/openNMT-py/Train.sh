67505bcc64f2cc116fbb99a15f6700e38c0da5eb
Jan Niehues <jan.niehues@kit.edu>
2018-01-15 15:34:56 +0000

    openNMT-py Training

--- scripts/openNMT-py/Train.sh
+++ scripts/openNMT-py/Train.sh
@@ -17,10 +17,10 @@ do
 	echo "" > /tmp/${name}/$set.$l
 	for f in /data/${input}/${set}/*\.${l}
 	do
-	    
+    
 		 cat $f >> /tmp/${name}/$set.$l
 	done
-    done
+   done
 done
 
 
@@ -29,14 +29,14 @@ python /opt/OpenNMT-py/preprocess.py \
        -train_src /tmp/${name}/train.s \
        -train_tgt /tmp/${name}/train.t \
        -valid_src /tmp/${name}/valid.s \
-       -valid_tgt /tmp/${name}/vaild.t \
+       -valid_tgt /tmp/${name}/valid.t \
        -save_data /tmp/${name}/train \
        -src_langs s \
        -tgt_langs t 
 
 
 python -u /opt/OpenNMT-py/train.py  -data /tmp/${name}/train.train.pt \
-       -save_model /model/${name}/model \
+       -save_model /tmp/${name}/model \
        -brnn \
        -rnn_size 512 \
        -word_vec_size 512 \
@@ -47,3 +47,41 @@ python -u /opt/OpenNMT-py/train.py  -data /tmp/${name}/train.train.pt \
        -epochs 10 \
        -learning_rate 0.001 \
        -gpus 0
+
+for f in /tmp/mt/model_ppl_*.pt
+do
+    echo $f >> /tmp/${name}/list ;
+done
+
+
+best=`awk '{ppl=$0;gsub(/.tmp.mt.model_ppl_/,"",ppl);gsub(/_e[0-9]*.pt/,"",ppl); if(NR==1 || 1.0*ppl < 1.0*min){min=ppl;f=$0}}END{print f}' /tmp/${name}/list`
+
+python -u /opt/OpenNMT-py/train.py  -data /tmp/${name}/train.train.pt \
+       -save_model /tmp/${name}/cont.model \
+       -brnn \
+       -rnn_size 512 \
+       -word_vec_size 512 \
+       -batch_size 128 \
+       -max_generator_batches 16 \
+       -optim adam \
+       -dropout 0.2 \
+       -epochs 5 \
+       -learning_rate 0.000125 \
+       -train_from_state_dict $best \
+       -gpus 0
+       
+
+for f in /tmp/mt/cont.model_ppl_*.pt
+do
+    echo $f >> /tmp/${name}/list.cont ;
+done
+
+
+best=`awk '{ppl=$0;gsub(/.tmp.mt.cont.model_ppl_/,"",ppl);gsub(/_e[0-9]*.pt/,"",ppl); if(NR==1 || 1.0*ppl < 1.0*min){min=ppl;f=$0}}END{print f}' /tmp/${name}/list.cont`
+echo $best
+
+cp $best /model/$name/model.pt
+cp /tmp/$name/train.dict.s /model/$name/
+cp /tmp/$name/train.dict.t /model/$name/
+
+rm -r /tmp/${name}/
