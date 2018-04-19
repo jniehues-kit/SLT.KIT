9d16bd4d35812aa362313962fde002d51f763b4f
Matthias Sperber <matthias.sperber@kit.edu>
2018-04-19 15:51:05 +0200

    preliminary TEDLIUM 2 training scripts for XNMT

---
+++ scripts/dataCollection/TEDLIUM-v2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Carry out these steps manually:
+echo - download TEDLIUM from http://www-lium.univ-lemans.fr/en/content/ted-lium-corpus
+echo - extract to /data/orig/TEDLIUM_release2
+echo then, comment out these lines
+exit
+
+/opt/SLT.KIT/scripts/xnmt/prep-data.sh
---
+++ systems/las-tedlium2/Train.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/root/anaconda3/bin/python -m xnmt.xnmt_run_experiments --dynet-gpu /opt/SLT.KIT/systems/las-tedlium2/config.las-pyramidal.yaml
