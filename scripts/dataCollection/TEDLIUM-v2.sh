b79dd818fb51d3fac5b294e0cdf3b39b4931766c
Matthias Sperber <matthias.sperber@kit.edu>
2018-04-19 17:36:13 +0200

    some fixes to XNMT TEDLIUM2 setup

--- scripts/dataCollection/TEDLIUM-v2.sh
+++ scripts/dataCollection/TEDLIUM-v2.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-echo Carry out these steps manually:
-echo - download TEDLIUM from http://www-lium.univ-lemans.fr/en/content/ted-lium-corpus
-echo - extract to /data/orig/TEDLIUM_release2
-echo then, comment out these lines
-exit
+mkdir -p /data/orig/
+if [ ! -f /data/orig/TEDLIUM_release2/README ]; then
+  echo Carry out these steps manually:
+  echo - download TEDLIUM from http://www-lium.univ-lemans.fr/en/content/ted-lium-corpus
+  echo - extract to /data/orig/TEDLIUM_release2
+  echo then re-run this script
+  exit
+fi
 
-/opt/SLT.KIT/scripts/xnmt/prep-data.sh
+# need absolute paths here:
+tedliumdir=/data/orig/TEDLIUM_release2
+datadir=/data/tedlium2-xnmt
+mkdir -p $datadir
+
+##### convert to .wav #######
+
+for spl in dev test train
+do
+  for f in `ls $tedliumdir/$spl/sph/*.sph`
+  do
+    mkdir -p $datadir/wav/$spl
+    filename=$(basename "$f" .sph) 
+    sox -t sph -b 16 -e signed -r 16000 -c 1 $f $datadir/wav/$spl/$filename.wav
+  done
+done
+
+#### prepare database and transcripts #####
+mkdir $datadir/db/
+mkdir $datadir/transcript/
+
+python extract_db.py "$tedliumdir" "$datadir"
+cat $datadir/transcript/dev.char | sed "s/ //g" | sed "s/__/ /g" > $datadir/transcript/dev.words
+cat $datadir/transcript/test.char | sed "s/ //g" | sed "s/__/ /g" > $datadir/transcript/test.words
+cat $datadir/transcript/train.char | sed "s/ //g" | sed "s/__/ /g" > $datadir/transcript/train.words
+
+
