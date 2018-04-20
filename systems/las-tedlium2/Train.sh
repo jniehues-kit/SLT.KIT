1d5f44f52313314d9d32f7b26ea696eb51f1b963
Matthias Sperber <matthias.sperber@kit.edu>
2018-04-20 11:03:57 +0200

    reflect previous change in Train.sh

--- systems/las-tedlium2/Train.sh
+++ systems/las-tedlium2/Train.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-/root/anaconda3/bin/python -m xnmt.xnmt_run_experiments --dynet-gpu /opt/SLT.KIT/scripts/xnmt/config.las-pyramidal.yaml
+/root/anaconda3/bin/python -m xnmt.xnmt_run_experiments --dynet-gpu /opt/SLT.KIT/scripts/xnmt/config.las-pyramidal-preproc.yaml
+
+/root/anaconda3/bin/python -m xnmt.xnmt_run_experiments --dynet-gpu /opt/SLT.KIT/scripts/xnmt/config.las-pyramidal-train.yaml
 
