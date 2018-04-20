98da924e0b6fc6be11653250aa7606a2e1b6421b
Matthias Sperber <matthias.sperber@kit.edu>
2018-04-20 11:05:23 +0200

    don't request GPU for feature extraction

--- systems/las-tedlium2/Train.sh
+++ systems/las-tedlium2/Train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/root/anaconda3/bin/python -m xnmt.xnmt_run_experiments --dynet-gpu /opt/SLT.KIT/scripts/xnmt/config.las-pyramidal-preproc.yaml
+/root/anaconda3/bin/python -m xnmt.xnmt_run_experiments /opt/SLT.KIT/scripts/xnmt/config.las-pyramidal-preproc.yaml
 
 /root/anaconda3/bin/python -m xnmt.xnmt_run_experiments --dynet-gpu /opt/SLT.KIT/scripts/xnmt/config.las-pyramidal-train.yaml
 
