ad690cec72aba3ad1a13636f4f8cf7600b66c56a
Matthias Sperber <matthias.sperber@kit.edu>
2018-05-09 17:28:38 +0200

    extract speech features for test data

--- scripts/xnmt/make-test-db.sh
+++ scripts/xnmt/make-test-db.sh
@@ -11,6 +11,10 @@ else
     java -jar /opt/tools/lium_spkdiarization-8.4.1.jar --fInputMask=$1/wavs/$f.wav --sOutputMask=$1/wavs/$f.seg $f
     cat $1/wavs/$f.seg | grep --invert-match ";;" | awk '{print "- {\"wav\": \"$1/wavs/" $1 ".wav\", \"offset\":" $3/100 ", \"duration\":" ($4)/100 "}"}' >> $1/test-db.yaml
   done
+ 
+  cat /opt/SLT.KIT/scripts/extract-test-db.yaml | sed "s|{IN}|$1/test-db.yaml|g" | sed "s|{OUT}|$1/test-db.h5|g" > $1/extract-test-db.yaml
+
+  /root/anaconda3/bin/python -m xnmt.xnmt_run_experiments $1/extract-test-db.yaml
 
 fi
 
