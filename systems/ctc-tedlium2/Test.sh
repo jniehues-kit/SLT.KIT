bc5da340dff01d96acf752e6063952bd7933d966
Thomas Zenkel <thomas.zenkel@gmail.com>
2018-05-17 11:30:13 +0000

    Update Test.sh script to include preprocessing

--- systems/ctc-tedlium2/Test.sh
+++ systems/ctc-tedlium2/Test.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+set=$1
+
 BPE_STEPS=300
+
+#Download Data - if not there
+if [ ! -e /data/orig/eval/$set ]; then
+    mkdir -p /data/orig/eval/$set
+    cd /data/orig/eval/$set
+    wget http://i13pc106.ira.uka.de/~jniehues/IWSLT-SLT/data/eval/en-de/IWSLT-SLT.$set.en-de.tgz
+    tar -xzvf IWSLT-SLT.$set.en-de.tgz
+fi
+
+/opt/SLT.KIT/scripts/xnmt/make-test-db.sh /data/orig/eval/${set}/IWSLT.${set}/
 
 # Test
 mkdir -p /model/ctc/
