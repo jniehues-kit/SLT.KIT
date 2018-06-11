3e86e0d1d7480eea77189944461e3cc332677174
Matthias Sperber <matthias.sperber@kit.edu>
2018-06-11 17:23:36 +0200

    download trained LAS model

---
+++ systems/las-tedlium2/Download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO: needed / correct?
+#export systemName=las-tedlium2
+
+cd /
+wget http://i13pc106.ira.uka.de/~jniehues/SLT.KIT/eh/las-pyramidal.mod.tgz
+tar -xzvf las-pyramidal.mod.tgz
+
