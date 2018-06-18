6c3d87c8bd22cdbf01e2d49ddd41dbe5a238ae24
Matthias Sperber <matthias.sperber@kit.edu>
2018-06-18 12:59:51 +0200

    update las model location

--- systems/las-tedlium2/Download.sh
+++ systems/las-tedlium2/Download.sh
@@ -3,8 +3,9 @@
 # TODO: needed / correct?
 #export systemName=las-tedlium2
 
+mkdir -p /model
 cd /
 wget http://i13pc106.ira.uka.de/~jniehues/SLT.KIT/en/las-pyramidal.mod.tgz
 tar -xzvf las-pyramidal.mod.tgz
-mv models/ /opt/SLT.KIT/scripts/xnmt/
+mv models /model/las
 
