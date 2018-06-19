289d69ff964d5c554104adf0090d0244b43fe4ff
Thomas Zenkel <thomas.zenkel@kit.edu>
2018-06-19 16:21:18 +0000

    Update ctc train recipe to deal with subword-nmt changes

--- systems/ctc-tedlium2/Train.sh
+++ systems/ctc-tedlium2/Train.sh
@@ -15,17 +15,31 @@ if [ ! -f /data/tedlium2-xnmt/feat/train.h5 ]; then
   /root/anaconda3/bin/python -m xnmt.xnmt_run_experiments /opt/SLT.KIT/scripts/xnmt/config.las-pyramidal-preproc.yaml  
 fi
 
+# remove junk that annoys python
+for data in "test" "dev" "train"
+do
+  tr -cd "[:print:]\n" < /data/tedlium2-xnmt/transcript/${data}.words > /data/tedlium2-xnmt/transcript/${data}.words.printable
+done
 # learn bpe
-/root/anaconda3/bin/python /opt/subword-nmt/subword_nmt/subword_nmt.py learn-bpe --input /data/tedlium2-xnmt/transcript/train.words --output /data/tedlium2-xnmt/bpe${BPE_STEPS}/bpe.rules --symbols ${BPE_STEPS}
+subword-nmt learn-bpe --input /data/tedlium2-xnmt/transcript/train.words.printable --output /data/tedlium2-xnmt/bpe${BPE_STEPS}/bpe.rules --symbols ${BPE_STEPS}
 
 # apply bpe
 for data in "test" "dev" "train"
 do
-  /root/anaconda3/bin/python /opt/subword-nmt/subword_nmt/subword_nmt.py apply-bpe -c /data/tedlium2-xnmt/bpe${BPE_STEPS}/bpe.rules < /data/tedlium2-xnmt/transcript/${data}.words > /data/tedlium2-xnmt/bpe${BPE_STEPS}/${data}.units
+  subword-nmt apply-bpe -c /data/tedlium2-xnmt/bpe${BPE_STEPS}/bpe.rules < /data/tedlium2-xnmt/transcript/${data}.words.printable > /data/tedlium2-xnmt/bpe${BPE_STEPS}/${data}.units
+done
+rm /data/tedlium2-xnmt/transcript/*.words.printable
+
+# create units for all characters, including base characters that are not necessarly used
+cat /data/tedlium2-xnmt/bpe${BPE_STEPS}/*.units > /data/tedlium2-xnmt/bpe${BPE_STEPS}/all.units.tmp
+characters="0123456789abcdefghijklmnopqrstuvwxyz'&-/"
+for i in $(seq 1 ${#characters});
+do
+  echo "${characters:i-1:1}\n" >> /data/tedlium2-xnmt/bpe${BPE_STEPS}/all.units.tmp
+  echo "${characters:i-1:1}@@\n" >> /data/tedlium2-xnmt/bpe${BPE_STEPS}/all.units.tmp
 done
 
 # write mapping of units to ids
-cat /data/tedlium2-xnmt/bpe${BPE_STEPS}/*.units > /data/tedlium2-xnmt/bpe${BPE_STEPS}/all.units.tmp
 /root/anaconda3/bin/python /opt/SLT.KIT/scripts/ctc/create_unit_dict.py --text /data/tedlium2-xnmt/bpe${BPE_STEPS}/all.units.tmp --output /model/ctc/units${BPE_STEPS}.json
 rm /data/tedlium2-xnmt/bpe${BPE_STEPS}/all.units.tmp
 
