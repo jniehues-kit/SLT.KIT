824a7bb9f8ceb706c0773d717b7f901a10034b32
tzenkel <thomas.zenkel@kit.edu>
2018-07-03 16:13:31 +0000

    Prevent throwing away segments after doing speaker diarization

--- scripts/xnmt/make-test-db.sh
+++ scripts/xnmt/make-test-db.sh
@@ -8,8 +8,10 @@ else
   echo "" > $1/test-db.yaml
   for f in `cat $1/FILE_ORDER`
   do
-    java -jar /opt/lium_spkdiarization-8.4.1.jar --fInputMask=$1/wavs/$f.wav --sOutputMask=$1/wavs/$f.seg $f
-    cat $1/wavs/$f.seg | grep --invert-match ";;" | awk '{print "- {\"wav\": \"PATH/wavs/" $1 ".wav\", \"offset\":" $3/100 ", \"duration\":" ($4)/100 "}"}' >> $1/test-db.yaml
+    java -jar /opt/lium_spkdiarization-8.4.1.jar --fInputSpeechThr=0.0 --fInputMask=$1/wavs/$f.wav --sOutputMask=$1/wavs/$f.seg $f --saveAllStep
+    # using *.s.seg for now, we live with possibly bad segmentation instead of throwing away to much stuff
+    # also sort by start offset of utterance
+    cat $1/wavs/$f.s.seg | grep --invert-match ";;" | sort -n -k3 | awk '{print "- {\"wav\": \"PATH/wavs/" $1 ".wav\", \"offset\":" $3/100 ", \"duration\":" ($4)/100 "}"}' >> $1/test-db.yaml
   done
   sed -i 's\PATH\'$1'\g' ${1}/test-db.yaml
  
