cbdd7aa266b6ec83888d124ec7d159e229c575f9
tzenkel <thomas.zenkel@kit.edu>
2018-07-04 11:28:10 +0000

    Write more scoring stats when using sclite

--- scripts/evaluate/Eval.asr.sh
+++ scripts/evaluate/Eval.asr.sh
@@ -14,5 +14,5 @@ mkdir -p /tmp
 # hacky way of scoring, just write the whole text in a line and give it the same utt-id
 grep "<seg id" /data/orig/eval/${testset}/*/*${testset}.en-de.en.xml | sed -e "s/<[^>]*>//g" -e "s/[\.?,:\!]//g" -e 's/"//g' -e "s/-//g" | tr '\n' ' ' | awk '{print $0 "(uttID-"NR")"}' > /tmp/${testset}.ref.trn
 cat ${hypothesis} | tr '\n' ' ' | awk '{print $0 "(uttID-"NR")"}' > /tmp/${testset}.hyp.trn
-/opt/sctk-2.4.10/bin/sclite -r /tmp/${testset}.ref.trn trn -h /tmp/${testset}.hyp.trn trn -i rm -o all -O $3
+/opt/sctk-2.4.10/bin/sclite -r /tmp/${testset}.ref.trn trn -h /tmp/${testset}.hyp.trn trn -i rm -o sum rsum sgml lur dtl pra prf -O $3
 
