5b40eea2de9796fee1953a8c7ff9b2e47f5053ca
tzenkel <thomas.zenkel@kit.edu>
2018-07-06 14:35:26 +0000

    Evaluate multiple ctc models

--- systems/ctc-tedlium2/Test.sh
+++ systems/ctc-tedlium2/Test.sh
@@ -4,7 +4,9 @@ if (( $# != 1 )); then
   echo "Specify a test set as a parameter to this script (e.g. tst2010)"
   exit
 fi
-BPE_SIZE=10000
+
+declare -a BPE_SIZES=("300" "10000")
+BEST_SIZE="10000"
 
 set=$1
 
@@ -22,19 +24,25 @@ if [ ! -e ${DATA_PATH}/test-db.h5 ]; then
     /opt/SLT.KIT/scripts/xnmt/make-test-db.sh /data/orig/eval/${set}/IWSLT.${set}/
 fi
 
-# Download model if is not there yet
-if [ ! -e /model/ctc/bpe${BPE_SIZE}.mdl ]; then
+for BPE_SIZE in "${BPE_SIZES[@]}"
+do
+
+  # Download model if is not there yet
+  if [ ! -e /model/ctc/bpe${BPE_SIZE}.mdl ]; then
     /opt/SLT.KIT/systems/ctc-tedlium2/Download.sh    
-fi
+  fi
 
-# Test
-CTC_OUTPUT=/data/ctc/eval/
-mkdir -p ${CTC_OUTPUT}
-/root/anaconda3/bin/python /opt/CTC.ISL/test.py /opt/SLT.KIT/scripts/ctc/bpe${BPE_SIZE}.yaml --logits_file ${CTC_OUTPUT}/${set}.logits --hyp_file ${CTC_OUTPUT}/${set}.s --model /model/ctc/bpe${BPE_SIZE}.mdl --audio_features ${DATA_PATH}/test-db.h5
+  # Test
+  CTC_OUTPUT=/data/ctc/eval/
+  mkdir -p ${CTC_OUTPUT}
+  /root/anaconda3/bin/python /opt/CTC.ISL/test.py /opt/SLT.KIT/scripts/ctc/bpe${BPE_SIZE}.yaml --logits_file ${CTC_OUTPUT}/${set}-${BPE_SIZE}.logits --hyp_file ${CTC_OUTPUT}/${set}-${BPE_SIZE}.s --model /model/ctc/bpe${BPE_SIZE}.mdl --audio_features ${DATA_PATH}/test-db.h5
 
-# normalize output inplace
-sed -i "s/ '/'/g" ${CTC_OUTPUT}/${set}.s
+  # normalize output inplace
+  sed -i "s/ '/'/g" ${CTC_OUTPUT}/${set}-${BPE_SIZE}.s
 
-mkdir -p ${CTC_OUTPUT}/${set}
-/opt/SLT.KIT/scripts/evaluate/Eval.asr.sh $set ${CTC_OUTPUT}/${set}.s ${CTC_OUTPUT}/${set}
+  mkdir -p ${CTC_OUTPUT}/${set}
+  /opt/SLT.KIT/scripts/evaluate/Eval.asr.sh $set ${CTC_OUTPUT}/${set}.s ${CTC_OUTPUT}/${set}
+done
+
+ln -s ${CTC_OUTPUT}/${set}-${BEST_SIZE}.s ${CTC_OUTPUT}/${set}.s
 
